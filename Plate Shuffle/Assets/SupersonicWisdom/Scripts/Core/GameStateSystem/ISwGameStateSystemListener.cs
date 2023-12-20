227567f0ac2896f6894d573a6050bf63ab821758
ibrarmayo <99178076+ibrarmayo@users.noreply.github.com>
2023-12-20 11:33:31 +0500

    setting

--- Plate Shuffle/Assets/SupersonicWisdom/Scripts/Core/GameStateSystem/ISwGameStateSystemListener.cs
+++
@@ -1,7 +0,0 @@
-namespace SupersonicWisdomSDK
-{
-    internal interface ISwGameStateSystemListener
-    {
-        void HandleGameSystemStateChange(object sender, SwSystemStateEventArgs eventArgs);
-    }
-}
--- Plate Shuffle/Assets/_GameMechanics/Zohaib Data/DataHolder.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class DataHolder : ScriptableObject
-{
-	public virtual void ClearData()
-	{
-	}
-}
